a08b37c75dc9027dab7fa77c731f81af24a26ef7
M Yusuf Ghazali <97452692+YusufTech123@users.noreply.github.com>
2023-09-21 11:47:16 +0700

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector(".menuToggle input");
-const nav = document.querySelector("nav ul");
-
-menuToggle.addEventListener("click", () => {
-  nav.classList.toggle("slide");
-});
